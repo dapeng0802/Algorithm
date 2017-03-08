7c3070211ccdfd0cc1f751462b375978404c171f
rendp <dapeng0802@126.com>
2017-03-09 00:09:13 +0800

    Question6_PrintKMajor

--- Array_and_Matrix/Question6_PrintHalfMajor.java
+++ Array_and_Matrix/Question6_PrintHalfMajor.java
@@ -30,3 +30,69 @@ public void printHalfMajor(int[] arr) {
 		System.out.println("no such number.");
 	}
 }
+
+/**
+ * 【进阶】
+ *   给定一个整型数组 arr，再给定一个整数 K，打印所有出现次数大于 N/K 的数，如果没有
+ * 这样的数，打印提示信息。
+ * 【要求】
+ *   时间复杂度为 O(N*K)，额外空间复杂度为 O(K)。
+ */
+public void printKMajor(int[] arr, int K) {
+	if (K < 2) {
+		System.out.println("the value of K is invalid.");
+		return;
+	}
+	HashMap<Integer, Integer> cands = new HashMap<Integer, Integer>();
+	for (int i = 0; i != arr.length; i++) {
+		if (cands.containsKey(arr[i])) {
+			cands.put(arr[i], cands.get(arr[i]) + 1);
+		} else {
+			if (cands.size() == K - 1) {
+				allCandsMinusOne(cands);
+			} else {
+				cands.put(arr[i], 1);
+			}
+		}
+	}
+	HashMap<Integer, Integer> reals = getReals(arr, cands);
+	boolean hasPrint = false;
+	for (Entry<Integer, Integer> set : cands.entrySet()) {
+		Integer key = set.getKey();
+		if (reals.get(key) > arr.length / K) {
+			hasPrint = true;
+			System.out.println(key + " ");
+		}
+	}
+	System.out.println(hasPrint ? "" : "no such number.");
+}
+
+public void allCandsMinusOne(HashMap<Integer, Integer> map) {
+	List<Integer> removeList = new LinkedList<Integer>();
+	for (Entry<Integer, Integer> set : map.entrySet()) {
+		Integer key = set.getKey();
+		Integer value = set.getValue();
+		if (value == 1) {
+			removeList.add(key);
+		}
+		map.put(key, value - 1);
+	}
+	for (Integer removeKey : removeList) {
+		map.remove(removeKey);
+	}
+}
+
+public HashMap<Integer, Integer> getReals(int[] arr, HashMap<Integer, Integer> cands) {
+	HashMap<Integer, Integer> reals = new HashMap<Integer, Integer>();
+	for (int i = 0; i != arr.length; i++) {
+		int curNum = arr[i];
+		if (cands.containsKey(curNum)) {
+			if (reals.containsKey(curNum)) {
+				reals.put(curNum, reals.get(curNum) + 1);
+			} else {
+				reals.put(curNum, 1);
+			}
+		}
+	}
+	return reals;
+}
