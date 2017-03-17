5eed945586d12707336b44d888a03911a71eac75
rendp <dapeng0802@126.com>
2017-03-17 22:42:12 +0800

    Array_and_Matrix Question9_PrintUniqueTriad

--- Array_and_Matrix/Question9_PrintUniquePair.java
+++ Array_and_Matrix/Question9_PrintUniquePair.java
@@ -26,3 +26,41 @@ public void printUniquePair(int[] arr, int k) {
 		}
 	}
 }
+
+/**
+ * 【补充题目】
+ *   给定排序数组 arr 和整数 k，不重复打印 arr 中所有相加和为 k 的不降序三元组。
+ *   例如，arr=[-8,-4,-3,0,1,2,4,5,8,9]，k=10，打印结果为：
+ *   -4,5,9
+ *   -3,4,9
+ *   -3,5,8
+ *   0,1,9
+ *   0,2,8
+ *   1,4,5
+ */
+public void printUniqueTriad(int[] arr, int k) {
+	if (arr == null || arr.length < 3) {
+		return;
+	}
+	for (int i = 0; i < arr.length - 2; i++) {
+		if (i == 0 || arr[i] != arr[i - 1]) {
+			printReset(arr, i, i + 1, arr.length - 1, k - arr[i]);
+		}
+	}
+}
+
+public void printReset(int[] arr, int f, int l, int r, int k) {
+	while (l < r) {
+		if (arr[l] + arr[r] < k) {
+			l++;
+		} else if (arr[l] + arr[r] > k) {
+			r--;
+		} else {
+			if (l == f + 1 || arr[l - 1] != arr[l]) {
+				System.out.println(arr[f] + "," + arr[l] + "," + arr[r]);
+			}
+			l++;
+			r--;
+		}
+	}
+}
