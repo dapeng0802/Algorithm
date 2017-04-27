dbdc2eca8c947942cacfbaf8584b67767c2ddc5f
rendp <dapeng0802@126.com>
2017-04-27 21:23:19 +0800

    BinaryTree Question9_PrintByZigZag

--- BinaryTree/Question9_PrintByLevel.java
+++ BinaryTree/Question9_PrintByLevel.java
@@ -49,3 +49,52 @@ public void printByLevel(Node head) {
 	}
 	System.out.println();
 }
+
+public void printByZigZag(Node head) {
+	if (head == null)
+		return;
+	Deque<Node> dq = new LinkedList<Node>();
+	int level = 1;
+	boolean lr = true;
+	Node last = head;
+	Node nLast = null;
+	dq.offerFirst(head);
+	printLevelAndOrientation(level++, lr);
+	while (!dq.isEmpty()) {
+		if (lr) {
+			head = dq.pollFirst();
+			if (head.left != null) {
+				nLast = nLast == null ? head.left : nLast;
+				dq.offerLast(head.left);
+			}
+			if (head.right != null) {
+				nLast = nLast == null ? head.right : nLast;
+				dq.offerLast(head.right);
+			}
+		} else {
+			head = dq.pollLast();
+			if (head.right != null) {
+				nLast = nLast == null ? head.right : nLast;
+				dq.offerFirst(head.right);
+			}
+			if (head.left != null) {
+				nLast = nLast == null ? head.left : nLast;
+				dq.offerFirst(head.left);
+			}
+		}
+		System.out.print(head.value + " ");
+		if (head == last && !dq.isEmpty()) {
+			lr = !lr;
+			last = nLast;
+			nLast = null;
+			System.out.println();
+			printLevelAndOrientation(level++, lr);
+		}
+	}
+	System.out.println();
+}
+
+public void printLevelAndOrientation(int level, boolean lr) {
+	System.out.print("Level " + level + " from ");
+	System.out.print(lr ? "left to right: " : "right to left: ");
+}
