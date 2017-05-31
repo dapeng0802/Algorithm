1c1339ea9d2bd16948e9ae0a372ed0664a34fcfd
dapeng0802 <dapeng0802@126.com>
2017-05-31 21:12:18 +0800

    BinaryTree isBCT

--- BinaryTree/Question15_IsBST&IsCBT.java
+++ BinaryTree/Question15_IsBST&IsCBT.java
@@ -33,3 +33,31 @@ public boolean isBST(Node head) {
 	}
 	return res;
 }
+
+public boolean isCBT(Node head) {
+	if (head == null) {
+		return true;
+	}
+	Queue<Node> queue = new LinkedList<Node>();
+	boolean leaf = false;
+	Node l = null;
+	Node r = null;
+	queue.offer(head);
+	while (!queue.isEmpty()) {
+		head = queue.poll();
+		l = head.left;
+		r = head.right;
+		if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
+			return false;
+		}
+		if (l != null) {
+			queue.offer(l);
+		}
+		if (r != null) {
+			queue.offer(r);
+		} else {
+			leaf = true;
+		}
+	}
+	return true;
+}
