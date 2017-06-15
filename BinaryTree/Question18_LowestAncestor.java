00725baf8437ddd4b43ad9e7ce3e72bd5842c0dd
dapeng0802 <dapeng0802@126.com>
2017-06-15 23:26:12 +0800

    Question18

--- BinaryTree/Question18_LowestAncestor.java
+++ BinaryTree/Question18_LowestAncestor.java
@@ -25,3 +25,44 @@ public Node lowestAncestor(Node head, Node o1, Node o2) {
 		return head;
 	return left != null ? left : right;
 }
+
+/**
+ * 进阶：如果查询两个节点的最近公共祖先的操作十分频繁，想法让单条查询的查询时间减少。
+ */
+class Record1 {
+	private HashMap<Node, Node> map;
+	
+	public Record1(Node head) {
+		map = new HashMap<Node, Node>();
+		if (head != null) {
+			map.put(head, null);
+		}
+		setMap(head);
+	}
+	
+	private void setMap(Node head) {
+		if (head == null) {
+			return;
+		}
+		if (head.left != null) {
+			map.put(head.left, head);
+		}
+		if (head.right != null) {
+			map.put(head.right, head);
+		}
+		setMap(head.left);
+		setMap(head.right);
+	}
+	
+	public Node query(Node o1, Node o2) {
+		HashSet<Node> path = new HashSet<Node>();
+		while (map.containsKey(o1)) {
+			path.add(o1);
+			o1 = map.get(o1);
+		}
+		while (!path.contains(o2)) {
+			o2 = map.get(o2);
+		}
+		return o2;
+	}
+}
